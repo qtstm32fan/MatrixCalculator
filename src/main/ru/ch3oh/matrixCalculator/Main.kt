e4951f8663616560ac91a0a21780303612da42e6
Egor Erastov <qtstm32fan@qtstm32fan.ru>
2022-10-11 19:57:11 +0300

    Rewrite in kotlin

---
+++ src/main/ru/ch3oh/matrixCalculator/Main.kt
@@ -0,0 +1,82 @@
+package ru.ch3oh.matrixCalculator
+
+fun main() {
+    println("Доступные действия для матриц(ы):")
+    println()
+    println("1) Сумма матриц")
+    println("2) Разность матриц")
+    println("3) Умножение матрицы на число")
+    println("4) Умножение матрицы на матрицу")
+    println("5) Транспонирование матрицы")
+    println("6) Определитель матрицы")
+    println("7) Ранг матрицы")
+    println("8) Обратная матрица")
+    println("9) Решение СЛАУ")
+    println()
+    print("Выберите действие: ")
+    val action: Action = when (readln().toInt()) {
+        1 -> Action.SUM
+        2 -> Action.SUBTRACTION
+        3 -> Action.MULTIPLICATION_BY_NUMBER
+        4 -> Action.MULTIPLICATION
+        5 -> Action.TRANSPOSITION
+        6 -> Action.DETERMINANT
+        7 -> Action.RANK
+        8 -> Action.INVERSE_MATRIX
+        9 -> Action.SOLVE
+        else -> Action.MULTIPLICATION_BY_NUMBER
+    }
+    println()
+    print("Введите количество строк первой матрицы: ")
+    val firstMatrixRows: Int = readln().toInt()
+    print("Введите количество столбцов первой матрицы: ")
+    val firstMatrixColumns: Int = readln().toInt()
+    val firstMatrix = Matrix(firstMatrixRows, firstMatrixColumns)
+    var secondMatrix = Matrix(1,1)
+    var number = 1.0
+    println()
+    println("Введите элементы первой матрицы по порядку.")
+    firstMatrix.fill()
+    println()
+    println("Итоговая первая матрица:")
+    println(firstMatrix)
+    if (action === Action.MULTIPLICATION_BY_NUMBER) {
+        //1 матрица и 1 число
+        print("Теперь введите число, на которое надо умножить матрицу: ")
+        number = readln().toDouble()
+    } else if (action === Action.SOLVE) {
+        println("Ввесдите свободные члены: ")
+        val secondMatrixLines: Int = firstMatrixRows
+        val secondMatrixColumns = 1
+        secondMatrix = Matrix(secondMatrixLines, secondMatrixColumns)
+        secondMatrix.fill()
+        println()
+        println("Итоговая вторая матрица:")
+        println(secondMatrix)
+    } else {
+        //2 матрицы
+        print("Введите количество строк второй матрицы: ")
+        val secondMatrixLines: Int = readln().toInt()
+        print("Введите количество столбцов второй матрицы: ")
+        val secondMatrixColumns: Int = readln().toInt()
+        secondMatrix = Matrix(secondMatrixLines, secondMatrixColumns)
+        println("Введите элементы второй матрицы по порядку.")
+        secondMatrix.fill()
+        println()
+        println("Итоговая вторая матрица:")
+        println(secondMatrix)
+        println()
+        println("Итог: ")
+    }
+    when (action) {
+        Action.SUM -> println(firstMatrix + secondMatrix)
+        Action.SUBTRACTION -> println(firstMatrix - secondMatrix)
+        Action.MULTIPLICATION -> println(firstMatrix * secondMatrix)
+        Action.MULTIPLICATION_BY_NUMBER -> println(firstMatrix * number)
+        Action.TRANSPOSITION -> println(firstMatrix.transpose())
+        Action.DETERMINANT -> println(firstMatrix.determinant())
+        Action.INVERSE_MATRIX -> println(firstMatrix.inverse())
+        Action.RANK -> println(firstMatrix.rank())
+        Action.SOLVE -> println(firstMatrix.solve(secondMatrix))
+    }
+}
---
+++ src/main/ru/ch3oh/matrixCalculator/Matrix.kt
@@ -0,0 +1,208 @@
+package ru.ch3oh.matrixCalculator
+
+import java.text.DecimalFormat
+import kotlin.math.pow
+
+class Matrix (matrix: Array<Array<Double>>) {
+    constructor(lines: Int, columns : Int) : this(matrix = Array(lines) {Array(columns) {0.0} })
+    private var _matrix = matrix
+    private val format = DecimalFormat("#.#####")
+    private var indices = _matrix.indices
+    operator fun plus (b: Matrix): Matrix {
+        val result = Matrix(this.getRows(), this.getColumns())
+        for (i in _matrix.indices) {
+            for (j in this[0].indices) {
+                result[i][j] = this[i][j] + b[i][j]
+            }
+        }
+        return result
+    }
+    fun fill() {
+        for (i in this.indices) {
+            for (j in this[0].indices) {
+                print((i + 1).toString() + ";" + (j + 1) + ": ")
+                this[i][j] = readln().toDouble()
+            }
+        }
+    }
+    operator fun minus (b: Matrix): Matrix {
+        val result = Matrix(this.getRows(), this.getColumns())
+        for (i in _matrix.indices) {
+            for (j in this[0].indices) {
+                result[i][j] = this[i][j] - b[i][j]
+            }
+        }
+        return result
+    }
+    operator fun times (b: Double): Matrix {
+        val result = Matrix(this.getRows(), this.getColumns())
+        for (i in _matrix.indices) {
+            for (j in this[0].indices) {
+                result[i][j] = this[i][j] * b
+            }
+        }
+        return result
+    }
+    operator fun times (b: Int): Matrix {
+        val result = Matrix(this.getRows(), this.getColumns())
+        for (i in _matrix.indices) {
+            for (j in this[0].indices) {
+                result[i][j] = this[i][j] * b
+            }
+        }
+        return result
+    }
+    operator fun times (b: Matrix): Matrix {
+        if (this.getColumns() != b.getRows()) {
+            throw ArithmeticException("Mismatched matrix sizes")
+        }
+        val result = Matrix(this.getRows(), b.getColumns())
+        for (i in this.indices) {
+            for (j in b[0].indices) {
+                var cell = 0.0
+                for (k in b.indices) {
+                    cell += this[i][k] * b[k][j]
+                }
+                result[i][j] = cell
+            }
+        }
+        return result
+    }
+    operator fun get (i: Int): Array<Double> {
+        return _matrix[i]
+    }
+    fun getColumns(): Int {
+        return _matrix[0].size
+    }
+    fun getRows(): Int {
+        return _matrix.size
+    }
+    fun transpose(): Matrix {
+        val result = Matrix(this.getRows(), this.getColumns())
+        for (i in this.indices){
+            for (j in this[0].indices) {
+                result[i][j] = this[j][i]
+            }
+        }
+        return result
+    }
+    fun determinant(): Double {
+        var result = 0.0
+        if (getRows() != getColumns()) {
+            throw ArithmeticException("Not a square matrix")
+        }
+        if (this.getRows() == 1) {
+            result = this[0][0]
+        } else if (this.getRows() == 2) {
+            result = this[0][0] * this[1][1] - this[0][1] * this[1][0]
+        } else {
+            for (i in this[0].indices) {
+                result += this[0][i] * complement(0, i)
+            }
+        }
+        return result
+    }
+    fun minor(i: Int, j: Int): Double {
+        val result: Double
+        val tempMatrix = Matrix(this.getRows() - 1, this.getColumns() - 1)
+        var flagRow = 0
+        var flagColumn: Int
+        for (k in 0 until tempMatrix.getRows()) {
+            flagColumn = 0
+            for (l in 0 until tempMatrix.getColumns()) {
+                if (l == j) {
+                    flagColumn = 1
+                }
+                if (k == i) {
+                    flagRow = 1
+                }
+                tempMatrix[k][l] = this[k + flagRow][l + flagColumn]
+            }
+        }
+        result = tempMatrix.determinant()
+        return result
+    }
+    fun complement(i: Int, j: Int): Double{
+        return (-1.0).pow(i+j+2)*minor(i,j)
+    }
+    fun inverse(): Matrix {
+        val determinant = this.determinant()
+        if (determinant == 0.0) {
+            throw ArithmeticException("Determinant can't be 0")
+        }
+        if (this.getRows() != this.getColumns()) {
+            throw ArithmeticException("Not a square matrix")
+        }
+        val compMatrix = Matrix(this.getRows(), this.getColumns())
+        for (i in 0 until this.getRows()) {
+            for (j in 0 until this.getColumns()) {
+                compMatrix[i][j] = complement(i, j)
+            }
+        }
+        return compMatrix.transpose() * (1 / determinant)
+    }
+    fun swap(row1: Int, row2: Int, column: Int){
+        for (i in 0..column) {
+            val temp: Double = this[row1][i]
+            this[row1][i] = this[row2][i]
+            this[row2][i] = temp
+        }
+    }
+    fun rank(): Int{
+        var rank: Int = this.getRows()
+        val tempMatrix: Matrix = this
+        for (k in this.indices){
+            var row = 0
+            while (row < rank) {
+                if (tempMatrix[row][row] != 0.0) {
+                    for (col in this[0].indices) {
+                        if (col != row) {
+                            val multi: Double = tempMatrix[col][row] / tempMatrix[row][row]
+                            for (i in 0 until rank) {
+                                tempMatrix[col][i] = tempMatrix[col][i] - multi * tempMatrix[row][i]
+                            }
+                        }
+                    }
+                } else {
+                    var reduce = true
+                    for (i in row + 1 until this.getRows()) {
+                        if (tempMatrix[i][row] != 0.0) {
+                            swap(row, i, rank)
+                            reduce = false
+                            break
+                        }
+                    }
+                    if (reduce) {
+                        rank--
+                        for (i in 0 until this.getRows()) {
+                            tempMatrix[i][row] = tempMatrix[i][rank]
+                        }
+                        row--
+                    }
+                }
+                row++
+            }
+        }
+        return rank
+    }
+    fun solve (b: Matrix): Matrix {
+        if (this.determinant() == 0.0) {
+            throw ArithmeticException("Determinant is zero")
+        }
+        val inv = this.inverse()
+        if ((this.rank() != inv.rank()) or (this.rank() != this.getRows())) {
+            throw ArithmeticException("This system is undefined")
+        }
+        return inv * b
+    }
+    override fun toString(): String{
+        var result = ""
+        for (doubles in _matrix) {
+            for (i in doubles) {
+                result += format.format(i) + " "
+            }
+            result += "\n"
+        }
+        return result
+    }
+}
